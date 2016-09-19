3eeeeb0ec50eb53772710dab94861414329a8fdb
Marie Belcredi <Marie Belcredi>
2016-09-20 09:44:51 +1000

    updated for the exercise on week 3 lesson 1`:wq

---
+++ app/scripts/controllers.js
@@ -0,0 +1,105 @@
+'use strict';
+
+angular.module('confusionApp')
+.controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory){
+	$scope.tab = 1; 
+
+	$scope.filtText = '';
+	$scope.showDetails = false;
+
+	$scope.dishes = menuFactory.getDishes();
+
+	$scope.select = function(setTab) {
+		$scope.tab = setTab;
+
+		if (setTab === 2)
+		{
+			$scope.filtText = "appetizer";
+		}
+		else if (setTab === 3)
+		{
+			$scope.filtText = "mains";
+		}
+		else if (setTab === 4)
+		{
+			$scope.filtText = "dessert";
+		}
+		else
+		{
+			$scope.filtText = "";
+		}
+	};
+	$scope.isSelected = function(currentTab)
+	{
+		return ($scope.tab === currentTab);
+
+	};
+	$scope.toggleDetails = function()
+	{
+		$scope.showDetails = !$scope.showDetails;
+	};
+}])
+
+
+
+
+.controller('ContactController', ['$scope', function($scope){
+	$scope.feedback = {mychannel: "", firstName: "" , lastName: "", agree:false, email:""};
+	var channels = [{value:"tel", label:"Tel."},{value:"Email", label:"Email"}];
+	$scope.channels = channels;
+	$scope.invalidChannelSelection = false;
+
+
+}])
+.controller('FeedbackController', ['$scope', function($scope){
+	console.log($scope.feedback);
+	$scope.sendFeedback= function(){
+		console.log($scope.feedback);
+
+		if($scope.feedback.agree &&
+			$scope.feedback.mychannel === "")
+		{
+			$scope.invalidChannelSelection = true;
+			console.log('incorrect');
+		}
+		else
+		{
+			$scope.invalidChannelSelection = false;
+			$scope.feedback = {
+				mychannel:"", firstName: "",
+				lastName: "", agree: false, email:""};
+				$scope.feedback.mychannel = "";
+				$scope.feedbackForm.$setPristine();
+				console.log($scope.feedback);
+			}
+		};
+
+	}])
+.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+	var dish = menuFactory.getDish(3);
+
+	$scope.dish = dish;
+	$scope.newComment =  {author:"", rating: 5, comment:""};
+}])
+.controller('CommentController', ['$scope', function($scope){
+	$scope.$parent.newComment = {author: comment.author, rating: comment.rating, comment:comment.comment};
+	$scope.sendComment = function(){
+		if($scope.commentForm.$pristine ||
+			$scope.newComment.author === "" ||
+			$scope.newComment.comment === "")
+		{
+			
+		}
+		else
+		{
+			$scope.invalidChannelSelection = false;
+			$scope.$parent.newComment.date = new Date();
+			$scope.$parent.dish.comments.push($scope.$parent.newComment);
+				$scope.$parent.newComment = {author:"", rating: 5, comment:""};
+				$scope.commentForm.$setPristine();
+			}
+
+		};
+
+	}])
+;
